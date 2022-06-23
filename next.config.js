1f72738e975546b4da01e0f607512a3f497d4516
hongjun-lim <davidlim2222@hotmail.com>
2022-06-23 14:41:48 +0800

    tailwindcss setup

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
