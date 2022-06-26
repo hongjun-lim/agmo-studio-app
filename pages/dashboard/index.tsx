7877ff9dd1ef8ce735f7bcd03c93a2e8c402d3e4
hongjun-lim <davidlim2222@hotmail.com>
2022-06-27 01:00:45 +0800

    fix checkbox value

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,3 +1,25 @@
+import { useRouter } from 'next/router';
+
 export default function Dashboard(): JSX.Element {
-  return <div>Dashboard</div>;
+  const router = useRouter();
+  const logOut = () => {
+    localStorage.removeItem('@user');
+
+    router.push('/');
+  };
+  return (
+    <div>
+      <title>Dashboard</title>
+      <p className='text-center text-4xl'>Dashboard</p>
+      <div className='flex flex-row justify-center'>
+        <button
+          type='submit'
+          onClick={logOut}
+          className='mt-10 border-solid border-2 border-black rounded-lg px-3 py-2 bg-black text-white font-semibold'
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
   const context = require('../pages/api/login.json');
   const router = useRouter();
   const [isChecked, setIsChecked] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
 
   const ValidationSchema = Yup.object().shape({
     email: Yup.string().email('Email address format wrong').required('* Required field'),
@@ -45,14 +44,27 @@ export default function Home() {
     }
   }
 
+  function handleUserData(data: FormValues) {
+    if (data.email !== userData.email || data.password !== userData.password) {
+      alert('Email address or password incorrect');
+    } else {
+      alert('Login Success');
+      localStorage.setItem('@user', JSON.stringify(data.email));
+      sessionStorage.setItem('email', data.email);
+      setIsChecked(true);
+    }
+  }
+
   const onSubmit = async (values: FormValues) => {
     const result = await userData;
+    const user = localStorage.getItem('@user');
+
+    if (!user) {
+      let mail = 'testing@test.com';
+      let user = (document.getElementById('email') as HTMLInputElement).value;
+    }
 
     if (result) {
-      if (result.email === values.email) {
-        localStorage.setItem('@user', JSON.stringify(values.email));
-      }
-
       initialRoute();
 
       const timeout = setTimeout(() => {
@@ -64,59 +76,69 @@ export default function Home() {
 
   return (
     <Formik initialValues={{ email: '', password: '' }} onSubmit={onSubmit} validationSchema={ValidationSchema}>
-      <div className='flex flex-col lg:flex-row justify-start '>
-        <div className='bg-neutral-600 relative lg:w-7/12 h-48 lg:h-screen'>
-          <img src='/images/agmo-banner.svg' alt='' className='mix-blend-overlay absolute w-full h-full object-cover' />
-          <img
-            src='/images/agmo-logo.svg'
-            alt=''
-            className='absolute object-cover mt-14 ml-40 w-40 lg:mt-96 lg:ml-470 lg:w-auto'
-          />
-        </div>
-        <div className='lg:mx-10 lg:basis-1/4'>
-          <div className='font-semibold text-3xl px-10'>
-            <p className='pt-32 lg:pt-64 pb-3'>Hello,</p>
-            <p className='pb-16'>Welcome Back</p>
+      {({ values }) => (
+        <div className='flex flex-col lg:flex-row justify-start '>
+          <div className='bg-neutral-600 relative lg:w-7/12 h-48 lg:h-screen'>
+            <img
+              src='/images/agmo-banner.svg'
+              alt=''
+              className='mix-blend-overlay absolute w-full h-full object-cover'
+            />
+            <img
+              src='/images/agmo-logo.svg'
+              alt=''
+              className='absolute object-cover mt-14 ml-40 w-40 lg:mt-96 lg:ml-470 lg:w-auto'
+            />
           </div>
-          <form className='px-10 lg:px-0 lg:ml-10'>
-            <div className='flex flex-col justify-between'>
-              <Field
-                type='email'
-                name='email'
-                autoComplete='email'
-                className='rounded-md bg-blue-100 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
-              />
-              <ErrorMessage name='email' render={(msg) => <span className='text-red-400'>{msg}</span>} />
-              <Field
-                type='password'
-                name='password'
-                autoComplete='password'
-                className='rounded-md bg-blue-100 mt-5 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
-              />
-              <ErrorMessage name='password' render={(msg) => <span className='text-red-400'>{msg}</span>} />
+
+          <div className='lg:mx-10 lg:basis-1/4'>
+            <div className='font-semibold text-3xl px-10'>
+              <p className='pt-32 lg:pt-64 pb-3'>Hello,</p>
+              <p className='pb-16'>Welcome Back</p>
             </div>
-            <div className='flex flex-row justify-start relative'>
-              <Field
-                type='checkbox'
-                name='rememberMe'
-                className='mr-2 my-4 border-2'
-                onChange={handleCheckbox}
-                checked={isChecked}
-              />
-              <p className='mt-3 absolute left-7'>Remember Me</p>
-              <a href='#' className='no-underline mt-3 absolute right-0'>
-                Forget Your Password?
-              </a>
-            </div>
-            <button
-              type='submit'
-              className='mt-10 border-solid border-2 border-black rounded-lg px-3 py-2 bg-black text-white font-semibold'
-            >
-              Log In
-            </button>
-          </form>
+            <form className='px-10 lg:px-0 lg:ml-10'>
+              <div className='flex flex-col justify-between'>
+                <Field
+                  type='email'
+                  name='email'
+                  autoComplete='email'
+                  className='rounded-md bg-blue-100 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+                />
+                <ErrorMessage name='email' render={(msg) => <span className='text-red-400'>{msg}</span>} />
+                <Field
+                  type='password'
+                  name='password'
+                  autoComplete='password'
+                  className='rounded-md bg-blue-100 mt-5 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+                />
+                <ErrorMessage name='password' render={(msg) => <span className='text-red-400'>{msg}</span>} />
+              </div>
+              <div className='flex flex-row justify-start relative'>
+                <Field
+                  type='checkbox'
+                  name='rememberMe'
+                  className='mr-2 my-4 border-2'
+                  onChange={handleCheckbox}
+                  checked={isChecked}
+                />
+                <p className='mt-3 absolute left-7'>Remember Me</p>
+                <a href='#' className='no-underline mt-3 absolute right-0'>
+                  Forget Your Password?
+                </a>
+              </div>
+              <button
+                type='submit'
+                onClick={() => {
+                  handleUserData(values);
+                }}
+                className='mt-10 border-solid border-2 border-black rounded-lg px-3 py-2 bg-black text-white font-semibold'
+              >
+                Log In
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </Formik>
   );
 }
